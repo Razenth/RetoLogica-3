b6e8f321ddbe8eb4fab37f31ecd8d26fcd5090f2
Razenth <santiago.gonzalezagudelo@gmail.com>
2023-12-04 18:22:49 -0500

    feat: :sparkles: Third Exercise done

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool flag = true;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("----------- MENÚ --------------");
+            Console.WriteLine("1. Verificar Par de números Amistosos");
+            Console.WriteLine("2. Mostrar Par de números Amistosos entre 1000 y 1500");
+            Console.WriteLine("3. Salir del Programa");
+            Console.WriteLine("-------------------------------------------");
+            string option = Console.ReadLine();
+            switch (option)
+            {
+                case "1":
+                    Console.Clear();
+                    System.Console.Write("Digite primer número: ");
+                    int number1 = int.Parse(Console.ReadLine());
+                    System.Console.Write("Digite segundo número: ");
+                    int number2 = int.Parse(Console.ReadLine());
+                    List<int> divisores1 = new List<int>();
+                    List<int> divisores2 = new List<int>();
+                    for (int i = 1; i < number1; i++)
+                    {
+                        if (number1 % i == 0)
+                        {
+                            divisores1.Add(i);
+                        }
+                    }
+                    for (int i = 1; i < number2; i++)
+                    {
+                        if (number2 % i == 0)
+                        {
+                            divisores2.Add(i);
+                        }
+                    }
+                    if (divisores1.Sum() == number2 && divisores2.Sum() == number1)
+                    {
+                        System.Console.WriteLine("{0} y {1} Son números amistosos :D", number1, number2);
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("{0} y {1} No son números amistosos :(", number1, number2);
+                    }
+                    break;
+
+                case "2":
+                Console.Clear();
+                    for (int i = 1000; i <= 1500; i++)
+                    {
+                        int result = SumaDivisores(i);
+                        if (result > i && SumaDivisores(result) == i)
+                        {
+                            System.Console.WriteLine("El par amistoso es: {0} Y {1}", i,result);
+                            break;
+                        }
+                    }
+                    static int SumaDivisores(int numero)
+                    {
+                        int suma = 1;
+                        for (int i = 2; i <= Math.Sqrt(numero); i++)
+                        {
+                            if (numero % i == 0)
+                            {
+                                suma += i;
+                                if (i != numero / i)
+                                {
+                                    suma += numero / i;
+                                }
+                            }
+                        }
+                        return suma;
+                    }
+                    break;
+                case "3":
+                System.Console.WriteLine("Adioos");
+                flag = false;
+                break;
+            }
+            System.Console.WriteLine("Toque una tecla para continuar");
+            Console.ReadLine();
+        } while (flag);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # RetoLogica-3
+## Reto Números Amistosos
+En este ejercicio implementé un menú que tiene 3 opciones
+```terminal
+----------- MENÚ --------------
+1. Verificar Par de números Amistosos
+2. Mostrar Par de números Amistosos entre 1000 y 1500
+3. Salir del Programa
+------------------------------------------- 
+```
+- Al digitar 1 la persona puede ingresar 2 datos para verificar si ese par son números amistosos
+
+Finalmente en la consola le mostrará si el par es amistoso
+
+```terminal
+Digite primer número: 5020
+Digite segundo número: 5564
+5020 y 5564 Son números amistosos :D
+```
+- Al digitar 2, le mostrará el par de números que existen entre el rango 1000 y 1500
+```terminal
+El par amistoso es: 1184 Y 1210
+```
+- Como última opcion, la persona podrá salir del programa 
